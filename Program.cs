b7d4db6319cfd9a9a00e1efaf8bd2a3581ec7a2d
sukhi107 <49531787+sukhi107@users.noreply.github.com>
2022-03-10 05:12:35 +0000

    Only committing Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.Write("Enter an amount to convert to coins: ");
+            string totalAsString = Console.ReadLine();
+            double total = Convert.ToDouble(totalAsString);
+
+            Console.WriteLine($"{total} cents is equal to...");
+
+            int goldValue = 10;
+            int silverValue = 5;
+
+            double goldCoins = Math.Floor(total / goldValue);
+            double remainder = total % goldValue;
+
+            double silverCoins = Math.Floor(remainder / silverValue);
+            remainder = remainder % silverValue;
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {remainder}");
+        }
+    }
+}
